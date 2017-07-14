2f35a2d98d9932d1870d2ee4ecfad568c1280c01
chris-allen <theone.theonly.me@gmail.com>
2017-07-14 12:13:51 -0400

    No longer adding sequelize cli globally

--- chef/recipes/system.rb
+++ chef/recipes/system.rb
@@ -11,11 +11,3 @@ package "git"
 
 # Install node and npm
 include_recipe "nodejs::npm"
-
-#Install sequelize cli globally
-nodejs_npm "sequelize-cli@2.5.1"
-
-# Add sequelize to path
-link '/usr/local/bin/sequelize' do
-  to "/usr/local/nodejs-#{node['nodejs']['install_method']}-#{node['nodejs']['version']}/bin/sequelize"
-end
